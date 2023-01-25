2e40f2724c6a52ea242af35587b60cbe92eacc5c
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-25 12:24:34 -0300

    getting geolocation from the device

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import {useEffect} from 'react';
 
-export const Map = () => {
+interface Props {
+  markers?: (typeof Marker)[];
+}
+
+export const Map = ({markers}: Props) => {
+  useEffect(() => {
+    // getCurrentPosition(success, error, options{})
+    Geolocation.getCurrentPosition(
+      info => console.log(info),
+      err => console.log(err),
+      {
+        distanceFilter: 100,
+        enableHighAccuracy: true,
+      },
+    );
+  }, []);
+
   return (
     <>
       <MapView
         style={{flex: 1}}
+        showsUserLocation
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}>
-        <Marker
+        {/* <Marker
           image={require('../assets/custom-marker.png')}
           coordinate={{
             latitude: 37.78825,
@@ -20,7 +39,7 @@ export const Map = () => {
           }}
           title="Title"
           description="Description"
-        />
+        /> */}
       </MapView>
     </>
   );
